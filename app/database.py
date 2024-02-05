0f25b5993c3f9b349d556ed28dfb8ff31d92eb5e
Felipe Parra <fpr29.10@gmail.com>
2024-02-04 22:21:29 -0600

    Fixed call config file

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from config import settings
+from .config import settings
 
 engine = create_engine(settings.database_url, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
